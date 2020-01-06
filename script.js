877be37c32310ef9e160d10909ba3aea58fd0a8a
Danielle <dani_1995@live.co.uk>
2020-01-06 18:43:39 +0000

    tip calculator almost finished

---
+++ script.js
@@ -0,0 +1,16 @@
+//user input chapture
+document.getElementById('container').onchange = function(){
+    const bill = Number(document.getElementById('total-bill').value);
+    const tipPercent = document.getElementById('tip-input').value;
+    const split = document.getElementById('split-input').value;
+
+    const tipValue = bill * (tipPercent/100);
+    const newBillEach = (bill + tipValue) / split;
+    const tipEach = tipValue / split;
+
+    document.getElementById('tip-output').innerHTML = `${tipPercent}%`;
+    document.getElementById('split-output').innerHTML = `${split}`;
+    document.getElementById('new-bill').innerHMTL = '£'+ newBillEach.toFixed(2);
+    document.getElementById('tip-each').innerHTML = '£' + tipEach.toFixed(2);
+};
+
